8a4cf9433d11404bec094771c82fc1eda4ae2b8b
Viery Nugroho <viery15102002@gmail.com>
2024-05-15 12:43:45 +0700

    update: setting for database deployment

--- config/database.js
+++ config/database.js
@@ -5,25 +5,43 @@ module.exports = {
 	development: {
 		username: DB_USER,
 		password: DB_PASSWORD,
-		database: `${DB_NAME}_development`,
+		database: DB_NAME,
 		host: DB_HOST,
 		port: DB_PORT,
 		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 	test: {
 		username: DB_USER,
 		password: DB_PASSWORD,
-		database: `${DB_NAME}_development`,
+		database: DB_NAME,
 		host: DB_HOST,
 		port: DB_PORT,
 		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 	production: {
 		username: DB_USER,
 		password: DB_PASSWORD,
-		database: `${DB_NAME}_development`,
+		database: DB_NAME,
 		host: DB_HOST,
 		port: DB_PORT,
 		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 };
